487bca714b99ee7170cda6836189695e3f41be9c
Ruslan M <evilpingu@yandex.ru>
2023-01-28 07:32:44 +0300

    brain_games.py have correct order of strings, but brain_even.py incorrect

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -7,3 +7,4 @@ def welcome_user():
         print('May I have your name? ', end='')
         name = input()
     print('Hello, ' + name + '!')
+    return name
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,10 @@
+from brain_games.scripts.braingames import main
+from brain_games.cli import welcome_user
+from random import randint
+
+question_even_number = randint(1, 100)
+print('Answer "yes" if the number is even, otherwise answer "no".')
+print('Question: ', question_even_number)
+answer = input()
+
+
