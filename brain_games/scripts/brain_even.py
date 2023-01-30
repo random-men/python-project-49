d04a4c3b7cefacc5d184fc2ff3d9e345efbedb3a
Ruslan M <evilpingu@yandex.ru>
2023-01-30 15:01:53 +0300

    fixed brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -9,18 +9,21 @@ def is_even(question_even_number):
         correct_answer = 'no'
     return correct_answer
 
-counter = 0
-while counter < 3:
-    question_even_number = randint(1, 100)
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    print('Question: ', question_even_number)
-    answer = input()
-    is_even(question_even_number) 
-    if answer != correct_answer:
-        print(f'"{answer}" is wrong answer. Correct answer was "{correct_answer}"')
-        break
+def game_even_go():
+    counter = 0
+    while counter < 3:
+        question_even_number = randint(1, 100)
+        print('Answer "yes" if the number is even, otherwise answer "no".')
+        print('Question: ', question_even_number)
+        answer = input()
+        is_even(question_even_number) 
+        if answer != correct_answer:
+            break
+            return print(f'"{answer}" is wrong answer. Correct answer was "{correct_answer}"')
+        else:
+            print('Correct!')
+            counter = counter + 1
     else:
-        print('Correct!')
-        counter = counter + 1
-else:
-    print('Congratulations! All 3 answers were correct!')
+        return print('Congratulations! All 3 answers were correct!')
+
+game_even_go()
