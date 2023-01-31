7076fe8b617645f25751202301455ac479ea182f
Ruslan M <evilpingu@yandex.ru>
2023-01-31 21:50:25 +0300

    Error with wrong order of printed lines has been fixed

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,6 @@
 from brain_games.cli import welcome_user
 from random import randint
 
-def main():
-    print('Welcome to the Brain Games!')
-    welcome_user()
 
 def is_even(question_even_number):
     global correct_answer
@@ -13,22 +10,30 @@ def is_even(question_even_number):
         correct_answer = 'no'
     return correct_answer
 
+
 def game_even_go():
     counter = 0
-    main()
     while counter < 3:
         question_even_number = randint(1, 100)
         print('Answer "yes" if the number is even, otherwise answer "no".')
         print('Question: ', question_even_number)
         answer = input()
-        is_even(question_even_number) 
+        is_even(question_even_number)
         if answer != correct_answer:
             print(f'"{answer}" is wrong answer. Correct answer was "{correct_answer}"')
-            return 
+            return
         else:
             print('Correct!')
             counter = counter + 1
     else:
         print('Congratulations! All 3 answers were correct!')
 
-game_even_go()
+
+def main():
+    print('Welcome to the Brain Games!')
+    welcome_user()
+    game_even_go()
+
+
+if __name__ == '__main__':
+    main()
