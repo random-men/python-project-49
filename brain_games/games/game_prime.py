7c428035d653f959cf06716d5051b30cb27d3845
Ruslan M <evilpingu@yandex.ru>
2023-02-10 01:46:56 +0300

    range (1,100) -> range (2,100)

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -4,12 +4,12 @@ GAME_RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def gen_quest_answer():
-    question = randint(1, 100)
+    question = randint(2, 100)
     counter_prime = 0
     for i in range(2, question):
         if question % i == 0:
             counter_prime = counter_prime + 1
-    if counter_prime <= 0:
+    if counter_prime <= 1:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
