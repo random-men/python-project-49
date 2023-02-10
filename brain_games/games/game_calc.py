b0e8c6f12f95cfa8d64f7174f465884d01a29119
Ruslan M <evilpingu@yandex.ru>
2023-02-10 16:19:13 +0300

    final fixing

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -10,7 +10,9 @@ def gen_quest_answer():
     question_calc_operation = ('+', '-', '*')
     choice_operation_index = randint(0, 2)
     final_calc_operation = question_calc_operation[choice_operation_index]
-    question = f'{question_calc_number1} {final_calc_operation} {question_calc_number2}'
+    question = f'{question_calc_number1} ' \
+               f'{final_calc_operation} ' \
+               f'{question_calc_number2}'
     if final_calc_operation == '+':
         correct_answer = question_calc_number1 + question_calc_number2
     elif final_calc_operation == '-':
